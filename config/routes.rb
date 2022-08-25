275cc48161ae5eb8123518b75e9e902ae4a78b58
lucasmendesdias <lucasbiocelp@gmail.com>
2022-08-25 16:50:19 -0300

    create the resources for bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'lists#home'
-  resources :lists, only: [:index, :new, :create, :show]
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create, :delete]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
